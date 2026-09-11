b0a87144e3046c1219f2440a1e860cc90b3ea8e1
Kitty Clark <kcd5393@ads.northwestern.edu>
2026-09-10 20:57:03 -0500

    Switch body font from Space Grotesk to Courier Prime

--- scripts/build-preview.js
+++ scripts/build-preview.js
@@ -121,7 +121,7 @@ const statsHTML = (site.stats || []).map(statTile).join('');
 const html = `<title>Don't Quit Productions</title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Anton&family=Space+Grotesk:wght@400;500;700&family=Permanent+Marker&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Anton&family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Permanent+Marker&display=swap" rel="stylesheet">
 <style>
 ${css}
 
